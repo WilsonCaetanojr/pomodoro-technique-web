cbfacac83bcf244f507e382d0f223999890957bd
Wilson Caetano <wilsonjr_caetano@hotmail.com>
2021-03-06 09:59:47 -0300

    Estrutura next

---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import { CompletedChallanges } from "../components/CompletedChallanges";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import { Countdown } from "../components/Countdown";
+import styles from "../styles/pages/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallanges />
+          <Countdown />
+        </div>
+        <div></div>
+      </section>
+    </div>
+  );
+}
